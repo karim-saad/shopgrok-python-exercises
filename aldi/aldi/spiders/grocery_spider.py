201392a2ce515241f7067c0b82cb0bdb2c5fb5e9
Karim <z5209523@ad.unsw.edu.au>
2021-02-28 13:53:50 +1100

    added text processing - all base products added - require following into deeper links

--- aldi/aldi/spiders/grocery_spider.py
+++ aldi/aldi/spiders/grocery_spider.py
@@ -6,12 +6,27 @@ class GrocerySpider(scrapy.Spider):
 
     start_urls = ['https://www.aldi.com.au/']
     
+    def process_price(self, product_price_value, product_price_decimal):
+        if product_price_value and product_price_decimal:
+            return product_price_value + product_price_decimal
+        elif not product_price_decimal and product_price_value:
+            return product_price_value + 'c'
+        else:
+            return None
+
     def parse(self, response):
-        for grocery_page in response.xpath('//body//div[@id="main"]/nav[@id="main-nav"]/ul/li[contains(@class, "product-range")]/div[contains(@class, "main-nav--level-container")]/ul/li/div/a'):
-            yield response.follow(grocery_page, callback=self.grocery_parse)
+        yield from response.follow_all(xpath='//body//div[@id="main"]/nav[@id="main-nav"]/ul/li[contains(@class, "product-range")]/div[contains(@class, "main-nav--level-container")]/ul/li/div/a', callback=self.grocery_parse)
 
     def grocery_parse(self, response):
-        for product in response.xpath('//body//div[@id="main"]/section/article/div[contains(@class, "csc-default")]/div[@class="tx-aldi-products"]/div/a'):
+        for product in response.xpath('//body//div[@id="main"]/section/article//div[@class="tx-aldi-products"]/div/a'):
+            product_title_raw = product.xpath('./div[@class="box m-text-image"]/div/div[contains(@class, "box--description")]/div[@class="box--description--header"]/text()').getall()
+            print(product_title_raw)
+            product_price_value = product.xpath('./div[@class="box m-text-image"]/div/div[contains(@class, "box--description")]/div[@class="box--price"]/span[@class="box--value"]/text()').get()
+            product_price_decimal = product.xpath('./div[@class="box m-text-image"]/div/div[contains(@class, "box--description")]/div[@class="box--price"]/span[@class="box--decimal"]/text()').get()
             yield {
-                'product-title': product.xpath('./div[@class="box m-text-image"]/div/div[contains(@class, "box--description")]/div[@class="box--description--header"]/text()').get().replace('\n', '').replace('\t', '')
+                'product_title': ''.join(product_title_raw).strip('\n\t '),
+                'product_image': product.xpath('./div[@class="box m-text-image"]/div/div[contains(@class, "box--image-container")]/img/@src').get(),
+                'packsize': product.xpath('./div[@class="box m-text-image"]/div/div[contains(@class, "box--description")]/div[@class="box--price"]/span[@class="box--amount"]/text()').get(),
+                'price': self.process_price(product_price_value, product_price_decimal),
+                'price_per_unit': product.xpath('./div[@class="box m-text-image"]/div/div[contains(@class, "box--description")]/div[@class="box--price"]/span[@class="box--baseprice"]/text()').get(),
             }
